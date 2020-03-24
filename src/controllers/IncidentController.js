fd0f30a19c43a268208ad7ad3b5ddde4d194a61b
cassianorsd <cassiano.rsd@gmail.com>
2020-03-24 15:22:52 -0300

    fixed all controllers

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -3,10 +3,20 @@ const connection = require('../database/connection');
 module.exports = {
     async list(req,res){
         const { page = 1 } = req.query;
+        const [count] = await connection('incidents').count();
         const incidents = await connection('incidents')
+        .join('ongs','ongs.id','=', 'incidents.ong_id')
         .limit(5)
         .offset((page-1)*2)
-        .select('*');
+        .select([
+            'incidents.*',
+            'ongs.name',
+            'ongs.email',
+            'ongs.whatsapp',
+            'ongs.city',
+            'ongs.uf'
+        ]);
+        res.header('X-Total-Count',count['count(*)'])
         return res.json(incidents)
     },
 
