39a153bd12539da2b3d6caf38cb3a770d5019519
cassianorsd <cassiano.rsd@gmail.com>
2020-03-25 14:10:43 -0300

    added incidents on home page

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './styles.css';
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 import {FiLogIn} from 'react-icons/fi'
-import { Link } from 'react-router-dom';
+import { Link,useHistory } from 'react-router-dom';
+import api from '../../services/api';
 
 export default function Logon(){
+    const [ id,setID] = useState('');
+    const history = useHistory();
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await api.post('/sessions',{id});
+            localStorage.setItem('ongId',id);
+            localStorage.setItem('ongName',response.data.name);
+            history.push('/profile')
+        } catch (error) {
+            alert('Falha no login, tente novamente.')
+        }
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Heroes"/>
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu logon</h1>
-                    <input placeholder="Sua ID"/>
-                    <button className="button" type='submit'>Entrar</button>
+                    <input 
+                        placeholder="Sua ID"
+                        vaue={id}
+                        onChange={e=>setID(e.target.value)}
+                        />
+                    <button 
+                        className="button" 
+                        type='submit' 
+                        >
+                            Entrar
+                    </button>
                     <Link  className='back-link' to="/register">
                         <FiLogIn size={16} color='#E02041'/>
                         Não tenho cadastro
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
 import {FiPower,FiTrash2} from 'react-icons/fi'
 import './styles.css'
+import api from '../../services/api';
 
 export default function Profile(){
+    const ongName = localStorage.getItem('ongName');
+    const ongId = localStorage.getItem('ongId');
+    const [ incidents, setIncidents ] = useState([]);
+
+    useEffect(()=>{
+        api.get('/profile',{
+            headers: {
+                Authorization:ongId
+            }
+        })
+        .then(res=>res.data)
+        .then(data=>{
+            setIncidents(data);
+        })
+        .catch(err=>{})
+    },[ongId])
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt='Be The hero'/>
-                <span>Bem vinda, APAD.</span>
+                <span>Bem vinda, {ongName}.</span>
                 <Link className='button' to='/incidents/new'>
                     Cadastrar novo caso
                 </Link>
@@ -19,50 +37,19 @@ export default function Profile(){
             </header>
             <h1>Casos cadastrados</h1>
             <ul>
-                <li>
+            {incidents.map(incident=>(
+                <li key={incident.id}>
                     <strong>CASO:</strong>
-                    <p>Caso teste</p>
+                    <p>{incident.title}</p>
                     <strong>DESCRIÇÃO</strong>
-                    <p>Descricao teste</p>
+                    <p>{incident.description}</p>
                     <strong>Valor:</strong>
-                    <p>R$: 120,00</p>
+                    <p>{incident.value}</p>
                     <button type="button">
                         <FiTrash2 size={20} color="#a8a8b3"/>
                     </button>
                 </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-                    <strong>DESCRIÇÃO</strong>
-                    <p>Descricao teste</p>
-                    <strong>Valor:</strong>
-                    <p>R$: 120,00</p>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-                    <strong>DESCRIÇÃO</strong>
-                    <p>Descricao teste</p>
-                    <strong>Valor:</strong>
-                    <p>R$: 120,00</p>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-                    <strong>DESCRIÇÃO</strong>
-                    <p>Descricao teste</p>
-                    <strong>Valor:</strong>
-                    <p>R$: 120,00</p>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3"/>
-                    </button>
-                </li>
+            ))}
             </ul>
         </div>
     )
