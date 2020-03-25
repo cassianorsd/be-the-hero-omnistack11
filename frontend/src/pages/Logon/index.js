9809754f3b5bd0433207250e8c4273ec6f98b152
cassianorsd <cassiano.rsd@gmail.com>
2020-03-25 12:30:10 -0300

    added react router

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import './styles.css';
-import heroesImg from '../../assets/heroes.png'
+import heroesImg from '../../assets/heroes.png';
+import logoImg from '../../assets/logo.svg';
+import {FiLogIn} from 'react-icons/fi'
 
 export default function Logon(){
     return (
-        <section className="form"></section>
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Heroes"/>
+                <form>
+                    <h1>Faça seu logon</h1>
+                    <input placeholder="Sua ID"/>
+                    <button className="button" type='submit'>Entrar</button>
+                    <a href="/register">
+                        <FiLogIn size={16} color='#E02041'/>
+                        Não tenho cadastro
+                    </a>
+                </form>
+            </section>
+            <img src={heroesImg} alt="Heroes"/>
+        </div>
     )
 }
