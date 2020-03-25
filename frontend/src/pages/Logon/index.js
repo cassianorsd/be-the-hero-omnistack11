a1f953921c0f4be38370a2f75da781566bf2b384
cassianorsd <cassiano.rsd@gmail.com>
2020-03-25 12:34:20 -0300

    added register page

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -3,6 +3,7 @@ import './styles.css';
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 import {FiLogIn} from 'react-icons/fi'
+import { Link } from 'react-router-dom';
 
 export default function Logon(){
     return (
@@ -13,10 +14,10 @@ export default function Logon(){
                     <h1>Faça seu logon</h1>
                     <input placeholder="Sua ID"/>
                     <button className="button" type='submit'>Entrar</button>
-                    <a href="/register">
+                    <Link to="/register">
                         <FiLogIn size={16} color='#E02041'/>
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
             <img src={heroesImg} alt="Heroes"/>
