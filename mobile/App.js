80c1e05ed2a870eb5a82400162171db300ccbb5a
cassianorsd <cassiano.rsd@gmail.com>
2020-03-26 10:03:12 -0300

    remove unwanted css

--- mobile/App.js
+++ mobile/App.js
@@ -3,20 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello OmniStack</Text>
+    <View>
+      <Text>Hello OmniStack</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title:{
-    color: '#FFF'
-  }
-});
